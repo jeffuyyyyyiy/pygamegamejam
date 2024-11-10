315d451cabb16d341b43de660ce71da1eeec74e3
jeffuyyyyyiy <jeffuyyyyyiy@gmail.com>
2024-11-09 20:55:44 -0500

    a
    a

--- main/main.py
+++ main/main.py
@@ -25,11 +25,15 @@ first_image = pygame.image.load('main\intro\intro1.png').convert_alpha()
 second_image = pygame.image.load('main\intro\intro2.png').convert_alpha()
 third_image = pygame.image.load('main\intro\intro3.png').convert_alpha()
 fourth_image = pygame.image.load('main\intro\intro4.png').convert_alpha()
+rain = pygame.mixer_music.load('main\Audio\intro_ambience.mp3')
 
 def playCutscene():
     cutscene_time = time.time()
+    textalpha = 0
+    firstalpha = 0
+    pygame.mixer_music.play()
     
-    while time.time() - cutscene_time < 10:
+    while time.time() - cutscene_time < 25:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -41,26 +45,39 @@ def playCutscene():
                     
         screen.fill("black")
         
-        text = cutscene_starting_text.render("test", False, (255,255,255))
+        text = cutscene_starting_text.render("\"It was once said HE slept peacefully.\"", False, (255,255,255))
         #just reuse this variable ^^
         
-        if time.time() - cutscene_time < 2.5:
+        if time.time() - cutscene_time < 5:
+            text.set_alpha(textalpha)
+            if textalpha < 255:
+                textalpha += 0.1
+            screen.blit(text, (400, 370))
+            pygame.display.flip()
+        elif time.time() - cutscene_time < 15:
             first_imagerescaled = pygame.transform.scale(first_image, (800,440))
             first_imagerescaled_rect = first_imagerescaled.get_rect()
+            first_imagerescaled.set_alpha(firstalpha)
+            text.set_alpha(textalpha)
+            if firstalpha < 255:
+                firstalpha += 0.1
+                
+            if firstalpha > 150 and textalpha > 0:
+                textalpha -= 0.1
             screen.blit(first_imagerescaled, first_imagerescaled_rect)
-            screen.blit(text, (400, 50))
+            screen.blit(text, (400, 370))
             pygame.display.flip()
-        elif time.time() - cutscene_time < 5:
+        elif time.time() - cutscene_time < 15.5:
             second_imagerescaled = pygame.transform.scale(second_image, (800,440))
             second_imagerescaled_rect = second_imagerescaled.get_rect()
             screen.blit(second_imagerescaled, second_imagerescaled_rect)
             pygame.display.flip()
-        elif time.time() - cutscene_time < 7.5:
+        elif time.time() - cutscene_time < 16:
             third_imagerescaled = pygame.transform.scale(third_image, (800,440))
             third_imagerescaled_rect = third_imagerescaled.get_rect()
             screen.blit(third_imagerescaled, third_imagerescaled_rect)
             pygame.display.flip()
-        elif time.time() - cutscene_time < 10:
+        elif time.time() - cutscene_time < 25:
             fourth_imagerescaled = pygame.transform.scale(fourth_image, (800,440))
             fourth_imagerescaled_rect = fourth_imagerescaled.get_rect()
             screen.blit(fourth_imagerescaled, fourth_imagerescaled_rect)
